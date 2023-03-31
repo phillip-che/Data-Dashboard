db44ef9dee46bfe418d2faccf9c590474288e2af
phillip-che <pche@cpp.edu>
2023-03-31 13:23:20 -0700

    Implemented function to fetch count genres

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,74 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
-import Card from './components/Card'
-import List from './components/List'
-import NavBar from './components/NavBar'
-import PieChart from './components/PieChart'
-const CLIENT_ID = import.meta.env.VITE_APP_CLIENT_ID
-const CLIENT_SECRET = import.meta.env.VITE_APP_CLIENT_SECRET
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Card from "./components/Card";
+import List from "./components/List";
+import NavBar from "./components/NavBar";
+import PieChart from "./components/PieChart";
+const CLIENT_ID = import.meta.env.VITE_APP_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_APP_CLIENT_SECRET;
 
 function App() {
-
   const [playlist, setPlaylist] = useState([]);
   const [filteredResults, setFilteredResults] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [followers, setFollowers] = useState(0);
   const [artistIDs, setArtistIDs] = useState([]);
-  const [genres, setGenres] = useState([]);
+  const [genres, setGenres] = useState([{}]);
 
   useEffect(() => {
     const callAPI = async () => {
-      await axios('https://accounts.spotify.com/api/token', {
+      await axios("https://accounts.spotify.com/api/token", {
         headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded',
-          'Authorization' : 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)      
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
         },
-        data: 'grant_type=client_credentials',
-        method: 'POST'
+        data: "grant_type=client_credentials",
+        method: "POST",
       }).then((tokenResponse) => {
-
         // gets playlist
-        axios('https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M', {
-          method: 'GET',
+        axios("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json',
-            'Authorization' : 'Bearer ' + tokenResponse.data.access_token
-          }
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + tokenResponse.data.access_token,
+          },
         }).then((playlistResponse) => {
           setPlaylist(playlistResponse.data.tracks.items);
           setFollowers(playlistResponse.data.followers.total);
           playlistResponse.data.tracks.items.forEach((item) => {
             let artistID = item.track.artists[0].id;
-            if(!artistIDs.includes(artistID)) {
+            if (!artistIDs.includes(artistID)) {
               setArtistIDs((artistIDs) => [...artistIDs, artistID]);
             }
           });
-        })
-
-        console.log(artistIDs);
+        });
 
         // get genres
-        // axios(`https://api.spotify.com/v1/playlists/https://api.spotify.com/v1/artists?ids=${artistIDs.join("")}`, {
-        //   method: 'GET',
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //     'Authorization' : 'Bearer ' + tokenResponse.data.access_token
-        //   }
-        // }).then((artistsResponse) => {
-        //   console.log(artistsResponse);
-        // })
-        
+        const getGenres = async () => {
+          await axios(
+            `https://api.spotify.com/v1/artists?ids=${artistIDs.join("%2C")}`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + tokenResponse.data.access_token,
+              },
+            }
+          ).then((artistsResponse) => {
+            const genreList = {};
+            artistsResponse.data.artists.forEach((artist) => {
+              artist.genres.forEach((genre) => {
+                genreList[genre] = genreList[genre] ? genreList[genre] + 1 : 1;
+              });
+            });
+            setGenres(genreList);
+          });
+        };
+        console.log(genres);
+        getGenres().catch(console.error);
       });
-    }
+    };
     callAPI().catch(console.error);
   }, []);
 
@@ -89,37 +97,37 @@ function App() {
           .includes(searchValue.toLowerCase())
       );
 
-      const filteredData = [...new Set([...filteredSongs, ...filteredAlbum, ...filteredArtist])];
+      const filteredData = [
+        ...new Set([...filteredSongs, ...filteredAlbum, ...filteredArtist]),
+      ];
       setFilteredResults(getSongs(filteredData));
     } else {
       setFilteredResults(playlist);
     }
-  }
+  };
 
   const getSongs = (keys) => {
     let results = [];
     keys.forEach((song) => {
       results.push(playlist[song]);
-    })
+    });
     return results;
-  }
+  };
 
   return (
     <div className="App">
       <div className="chart">
-        <PieChart />
+        <PieChart genres={genres} />
       </div>
       <div className="top-section">
-        <Card playlist={playlist} followers={followers}/>
+        <Card playlist={playlist} followers={followers} />
         <NavBar searchItems={searchItems} />
       </div>
-      <List playlist={
-        (searchInput.length > 0) ? filteredResults : playlist 
-      } />
+      <List playlist={searchInput.length > 0 ? filteredResults : playlist} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // filter by: genres
